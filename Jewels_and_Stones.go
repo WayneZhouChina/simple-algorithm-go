5f4f7ef49f472998bd5c711096670dd993429611
yuanyuan50 <yuanyuan50@staff.sina.com.cn>
2018-07-01 23:09:19 +0800

    [leetcode 771] Jewels and Stones

---
+++ Jewels_and_Stones.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+)
+
+func numJewelsInStones(J string, S string) int {
+	var count int = 0
+
+	ss := strings.Split(S, "")
+	for _, s := range ss {
+		if strings.Contains(J, s) == true {
+			count++
+		}
+	}
+
+	return count
+
+}
